4bb065786102ae720928b3080115f9cac33a33da
sunilkumarojha <100593013+sunilkumarojha@users.noreply.github.com>
2022-04-15 19:21:38 +0530

    String messages and test cases

--- src/com/bridgelabz/MoodAnalyser.java
+++ src/com/bridgelabz/MoodAnalyser.java
@@ -1,5 +1,20 @@
 package com.bridgelabz;
 
 public class MoodAnalyser {
-    
+    static String message;
+
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    public static String analyseMood() {
+        String messageOne = "I am in Sad Mood";
+        String messageTwo = "I am in any Mood";
+        if (message.equalsIgnoreCase(messageOne))
+            return "SAD";
+        else if (message.equalsIgnoreCase(messageTwo))
+            return "HAPPY";
+        else
+            return "No Match";
+    }
 }
