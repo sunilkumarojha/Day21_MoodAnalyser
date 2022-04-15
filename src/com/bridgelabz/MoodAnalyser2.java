29442b5c55843240b7f227c4b0d99bff0375179f
sunilkumarojha <100593013+sunilkumarojha@users.noreply.github.com>
2022-04-15 19:26:13 +0530

    Null exception and test cases

--- src/com/bridgelabz/MoodAnalyser2.java
+++ src/com/bridgelabz/MoodAnalyser2.java
@@ -1,5 +1,15 @@
 package com.bridgelabz;
 
-public class MoodAnalyser2 {
+public class MoodAnalyser {
 
+    public static String analyseMood(String message) {
+        String messageOne = "I am in Sad Mood";
+        String messageTwo = "I am in any Mood";
+        if (message.equalsIgnoreCase(messageOne))
+            return "SAD";
+        else if (message.equalsIgnoreCase(messageTwo))
+            return "HAPPY";
+        else
+            return "No Match";
+    }
 }
